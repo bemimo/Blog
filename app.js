9f13833ba80e8dd40e0deca33bd144f20dfcab62
Johannes Kordina <johannes.kordina@gmail.com>
2022-11-17 20:35:15 +0100

    Second Commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,29 +19,42 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
 
+// Database Cloud
+const database = module.export = () =>{
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  try{
+    mongoose.connect("mongodb+srv://mimo:PW@cluster0.h92lk4g.mongodb.net/mongodb?retryWrites=true&w=majority")
+    console.log("Connection to MongoDB was successfully established");
+  }catch (error){
+    console.log("Error no connection to DB")
+  }
+}
 
+const postSchema = {
+  title: String,
+  content: String
+};
 
+const Post = mongoose.model("Post", postSchema);
 
 app.get('/', (req, res) => {
-  res.render("home", {
-  startingContent: homeStartingContent,
-  posts: posts,
-    });
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      })
+    })
   });
+
 
 app.get("/About", (req, res) => {
   res.render("about", {
     aboutText: aboutContent,
   });
-});
-
-
-app.get("/Contact", (req, res) => {
-  res.render("contact", {
-    contactText: contactContent,
-  });  
 });
 
 app.get("/compose", (req, res) => {
@@ -50,30 +65,45 @@ app.get("/compose", (req, res) => {
 
 app.post("/compose", (req, res) => {
   
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-  posts.push(post);
-  res.redirect("/");
-  
-});
+  });
 
-app.get("/posts/:postName", (req, res) => {
-    const requestedTitle = _.toLower(req.params.postName);
-
-  posts.forEach((post) => {
-    const storedTitle = _.toLower(post.title);
-    if (storedTitle === requestedTitle)
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      })
-    })
+  post.save(function(err){
+    if(!err){
+    res.redirect("/");
+   } 
+  });
 })
 
+app.get("/Contact", (req, res) => {
+  res.render("contact", {
+    contactText: contactContent,
+  });  
+});
 
 
+
+
+
+
+
+
+app.get("/posts/:postId", (req, res) => {
+    
+  const requestedPostId = req.params.postId; 
+
+    Post.findOne({_id:requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+    });
+  });
+});
+
+
+database(),
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
